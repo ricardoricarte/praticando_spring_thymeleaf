5c3970753d04ceaf738932563f5b3e0f983407a6
ricardoricarte <dev.ricarte@gmail.com>
2022-12-13 00:59:21 -0300

    Spring e ThymeLeaf

---
+++ src/main/java/com/ricartedev/tasks/TaskApplication.java
@@ -0,0 +1,13 @@
+package com.ricartedev.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskApplication.class, args);
+	}
+
+}
